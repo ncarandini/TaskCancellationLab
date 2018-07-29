15eeba4bcdad5298e54e77303ebcfc82122e95f2
ncarandini <n.carandini@outlook.com>
2018-07-29 11:28:36 +0200

    Completed First lab

---
+++ TaskCancellationLab1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCancellationLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            CancellationToken ct = cts.Token;
+
+            var task = Task.Factory.StartNew(() =>
+            {
+                // Were we already canceled?
+                ct.ThrowIfCancellationRequested();
+
+                if (ct.IsCancellationRequested)
+                {
+                    Console.WriteLine("Cancellation requested before string task.");
+                    throw new OperationCanceledException();
+                }
+                else
+                {
+                    Console.WriteLine("Starting task...");
+                }               
+
+                bool moreToDo = true;
+                while (moreToDo)
+                {
+                    // Poll on this property if you have to do
+                    // other cleanup before throwing.
+                    if (ct.IsCancellationRequested)
+                    {
+                        // Clean up here, then...
+                        Console.WriteLine("Cancellation requested during task execution.");
+                        throw new OperationCanceledException();
+                    }
+                }
+            }, cts.Token); // Pass cancellation token to StartNew.
+
+            task.Wait(1000);
+            cts.Cancel();
+
+            // Just continue on this thread, or Wait/WaitAll with try-catch:
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException e)
+            {
+                foreach (var v in e.InnerExceptions)
+                    Console.WriteLine(e.Message + " " + v.Message);
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+
+            Console.Write("Hit a key to terminate...");
+            Console.ReadKey();
+        }
+    }
+}
--- TaskCancellationLab1/Properties/AssemblyInfo.cs
+++ TaskCancellationLab1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TaskCancellationLab")]
+[assembly: AssemblyTitle("TaskCancellationLab1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TaskCancellationLab")]
+[assembly: AssemblyProduct("TaskCancellationLab1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cc847c37-3f7d-48cd-8883-f6f3a8c36493")]
+[assembly: Guid("f9b9704b-b58c-4326-bce2-f3e7f19b3a73")]
 
 // Version information for an assembly consists of the following four values:
 //
